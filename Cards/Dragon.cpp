8b2249d802ae7cc102ce1b75ad0cca6cb08fceae
s-griffith <cmcgriffith@gmail.com>
2022-06-10 02:19:56 +0300

    Created and commented Dragon class

---
+++ Cards/Dragon.cpp
@@ -0,0 +1,20 @@
+#include "Dragon.h"
+#include "../Player.h"
+
+//Constructor
+Dragon::Dragon() : m_name("Dragon"), m_force(DRAGON_FORCE), m_loot(DRAGON_LOOT) {}
+
+//Apply Encounter Function
+void Dragon::applyEncounter(Player& player) const
+{
+    bool win = (player.getAttackStrength() >= m_force);
+    if (win) {
+        player.levelUp();
+        player.addCoins(m_loot);
+        printWinBattle(player.m_name, m_name);
+    }
+    else {
+        player.damage(player.m_maxHP);
+        printLossBattle(player.m_name, m_name);
+    }
+}
--- Cards/Dragon.h
+++ Cards/Dragon.h
@@ -6,10 +6,41 @@
 const int DRAGON_FORCE = 25;
 const int DRAGON_LOOT = 1000;
 
+//Issue with this class: Dragon has no real need for hpLossOnDefeat. Making that an internal field though will change the entire implementation of everything else.
+
+/*
+ *  Dragon Monster Class:
+ *  This class represents a single Dragon card.
+*/
 class Dragon : public MonsterCards {
 public:
-    //functions: applyencounter, print?
-    //make hpLoss = maxHP from player class, or just do hp = hp - hp?
+    /*
+     * Constructor of Dragon class
+     *
+     * Receives no parameters.
+     * @return
+     *      A new instance of Dragon with the appropriate values.
+    */
+    Dragon();
+
+    /*
+     * Destructor of Dragon class
+    */ 
+    ~Dragon() = default;
+
+    /*
+     * Copy constructor of Dragon class
+    */  
+    Dragon(const Dragon&) = default;
+
+    /*
+     * Handling the player's applyEncounter with a Dragon card:
+     *
+     * @param player - The player.
+     * @return
+     *      void
+    */
+    void applyEncounter(Player& player) const override;
 };
 
 #endif //EX4_DRAGON_H
