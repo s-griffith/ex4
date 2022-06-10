a6bb4a2c19daeab0ccda485bd8955a1a4b103101
s-griffith <cmcgriffith@gmail.com>
2022-06-10 16:58:50 +0300

    Added ; at the end of each class

--- Exception.h
+++ Exception.h
@@ -5,12 +5,12 @@
 class DeckFileNotFound : public std::exception {
 public:
     const char* what() const noexcept override;    
-}
+};
 
 class DeckFileInvalidSize : public std::exception {
 public:
     const char* what() const noexcept override;
-}
+};
 
 const char* DeckFileNotFound::what() const noexcept
 {
