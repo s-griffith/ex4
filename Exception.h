ce6fae070b31411bab6bcf6306a3179c92dfb372
s-griffith <cmcgriffith@gmail.com>
2022-06-10 16:55:17 +0300

    Created two of the three error classes

---
+++ Exception.h
@@ -0,0 +1,23 @@
+#include <iostream>
+
+//Why no cpp? Are we supposed to implement the functions directly inside the classes?
+
+class DeckFileNotFound : public std::exception {
+public:
+    const char* what() const noexcept override;    
+}
+
+class DeckFileInvalidSize : public std::exception {
+public:
+    const char* what() const noexcept override;
+}
+
+const char* DeckFileNotFound::what() const noexcept
+{
+    return "Deck File Error: File not found";
+}
+
+const char* DeckFileInvalidSize::what() const noexcept
+{
+    return "Deck File Error: Deck size is invalid";
+}
