8462705120181858e81ddf10dc6f6e06e4eb915d
s-griffith <cmcgriffith@gmail.com>
2022-06-15 21:04:07 +0300

    Try catch in merchant

--- Cards/Merchant.cpp
+++ Cards/Merchant.cpp
@@ -8,39 +8,36 @@ void Merchant::applyEncounter(Player& player) const
 {
     printMerchantInitialMessageForInteractiveEncounter(std::cout, player.getName(), player.getCoins());
     std::string initialInput;
-    int cost;
-    std::getline(std::cin, initialInput);
-    int input = std::stoi(initialInput);
-    while (input != 0 && input != 1 && input != 2) {
-        printInvalidInput();
-        std::getline(std::cin, initialInput);
-        input = std::stoi(initialInput);    
+    int cost = 0;
+    int input = -1;
+    bool inputValid = true;
+    do {
+        if (!inputValid || (input != 0 && input != 1 && input != 2)) {
+            printInvalidInput();
+        }
+        try {
+            std::getline(std::cin, initialInput);
+            input = std::stoi(initialInput);
+        } catch (std::exception&){
+            inputValid = false;
+        }
+    }  while (!inputValid || (input != 0 && input != 1 && input != 2));
+
+    if (input == 1 && player.pay(HEALTH_POTION_COST)) {
+        player.heal(HEALTH_POTION);
+        cost = HEALTH_POTION_COST;
     }
-    switch(input) {
-        case 0:
-            cost = input;
-            break;
-        case 1:
-            cost = HEALTH_POTION_COST;
-            break;
-        case 2:
-            cost = FORCE_BOOST_COST;
-            break;
+    else if (input == 2 && player.pay(FORCE_BOOST_COST)) {
+        player.buff(FORCE_BOOST);
+        cost = FORCE_BOOST_COST;
     }
-    if (!player.pay(cost)) {
-        cost = 0;
-        input = 0;
-        printMerchantInsufficientCoins(std::cout);
+    else if (input == 0) {
+        cost = input;
     }
     else {
-        switch(input) {
-            case 1:
-                player.heal(HEALTH_POTION);
-                break;
-            case 2:
-                player.buff(FORCE_BOOST);
-                break;
-        }
+        input = 0;
+        cost = input;
+        printMerchantInsufficientCoins(std::cout);
     }
     printMerchantSummary(std::cout, player.getName(), input, cost);
 }
